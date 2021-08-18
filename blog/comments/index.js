26bd17d672cf20b4e306e5cf6ae1db386aa6c764
Rachmat Hidayat <rhgenius@gmail.com>
2021-08-18 15:30:09 +0700

    update comments

---
+++ blog/comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostID = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostID[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentsID = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostID[req.params.id] || [];
+
+    comments.push({ id: commentsID, content });
+
+    commentsByPostID[req.params.id] = comments;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('listening on 4001');
+});
